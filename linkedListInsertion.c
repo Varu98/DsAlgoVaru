98c9235e7785ba3c91b48dbe33d11486e3dd9f2b
Vardaan <vardaan.agarwal.98@gmail.com>
2021-11-29 13:56:04 +0530

    first commit

---
+++ linkedListInsertion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+}
